c5fb0b039dcc2093de3477002e9b42b363f72b0f
Yvonne <wanguiy001@gmail.com>
2025-05-19 15:59:40 +0300

    added:   recap/lib/widgets/cartappbar.dart

--- recap/lib/pages/CartPage.dart
+++ recap/lib/pages/CartPage.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:recap/widgets/cartappbar.dart';
 
 class Cartpage extends StatelessWidget {
   const Cartpage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ListView(
+        children: [
+          CartAppBar(),
+        ],
+      ),
+    );
   }
 }
