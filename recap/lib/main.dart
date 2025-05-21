692bdf88048fe879ba87d919e2e080402956f292
Yvonne <wanguiy001@gmail.com>
2025-05-21 11:32:02 +0300

    added:   recap/lib/pages/ItemPage.dart

--- recap/lib/main.dart
+++ recap/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recap/pages/Itempage.dart';
 import 'package:recap/pages/homepage.dart';
 import 'package:recap/pages/CartPage.dart';
 
@@ -20,6 +21,9 @@ class MyApp extends StatelessWidget {
      routes: {
         '/': (context) => const Homepage(),
         '/CartPage': (context) => const Cartpage(),
+        '/ItemPage': (context) => const ItemPage(),
+
+
        
       },
   
